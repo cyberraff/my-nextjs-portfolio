485f5a49fed9a6bad70bbdf8e99c41ea7dcc5c7e
Cyber-Raff <cyberraff@gmail.com>
2022-08-02 08:55:23 +0100

    fixed some more typos

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -105,7 +105,7 @@ const PostDetail = ({ post }) => {
                                 </div>
                                 <div className='px-4 lg:px-0'>
                                     <div className='flex items-center mb-8 w-full'>
-                                        <div className='hidden md:flex items-center justify-center lg:mb-0 lg:w-auto mr-8 items-center'>
+                                        <div className='flex md:flex  justify-center lg:mb-0 lg:w-auto mr-8 items-center'>
                                             <img
                                                 alt={post.author.name}
                                                 className='align-middle rounded-full h-[30px] w-[30px]'
