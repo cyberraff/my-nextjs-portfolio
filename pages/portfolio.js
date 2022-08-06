5f0d89cdcf9b589e94032b03284ba431c38127be
cyberraff <50131328+cyberraff@users.noreply.github.com>
2022-08-06 10:49:16 +0100

    update portfolio.js

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -119,6 +119,6 @@ export async function getStaticProps() {
     const projects = await getProjects()
     return {
         props: { projects },
-        revalidate: 86400 // In one day
+        revalidate: 43200 // In 12 hrs
     }
 }
